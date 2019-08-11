ed542ec2985f2c67b5fc67d69f4ac55dc36bec3d
UnknowN-ag <ag528927@gmail.com>
2019-08-11 10:14:11 +0530

    3rd commit

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from shop.models import product
+from math import ceil
 
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products = product.objects.all()
+    print(products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_products':n, 'range':range(1,nSlides), 'product':products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    return HttpResponse('we are at about')
+    return render(request, 'shop/about.html')
 
 def contact(request):
     return HttpResponse('we are at contact')
